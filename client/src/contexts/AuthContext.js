4ef68d5dd5d3282467634519886eaa9bdc7de99a
NguyenLoi851 <nguyensyloi851@gmail.com>
2022-04-06 12:51:48 +0700

    load user

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -2,7 +2,7 @@ import { createContext, useReducer, useEffect } from 'react'
 import { authReducer } from '../reducers/authReducer'
 import { apiUrl, LOCAL_STORAGE_TOKEN_NAME } from './constants'
 import axios from 'axios'
-
+import setAuthToken from '../components/utils/setAuthToken'
 
 export const AuthContext = createContext()
 
@@ -13,7 +13,31 @@ const AuthContextProvider = ({ children }) => {
 		user: null
 	})
 
+		// Authenticate user
+		const loadUser = async () => {
+			if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
+				setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME])
+			}
 	
+			try {
+				const response = await axios.get(`${apiUrl}/auth`)
+				if (response.data.success) {
+					dispatch({
+						type: 'SET_AUTH',
+						payload: { isAuthenticated: true, user: response.data.user }
+					})
+				}
+			} catch (error) {
+				localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME)
+				setAuthToken(null)
+				dispatch({
+					type: 'SET_AUTH',
+					payload: { isAuthenticated: false, user: null }
+				})
+			}
+		}
+	
+		useEffect(() => loadUser(), [])
 
 	// Login
 	const loginUser = async userForm => {
@@ -34,7 +58,7 @@ const AuthContextProvider = ({ children }) => {
 
 	
 	// Context data
-	const authContextData = { loginUser }
+	const authContextData = { loginUser, loadUser, authState }
 
 	// Return provider
 	return (
